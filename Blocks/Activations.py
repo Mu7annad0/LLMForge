591079a8d705e077dd422cf5088c60fac78cea30
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-10-17 04:34:47 +0300

    Adding SiLU activation function

--- Blocks/Activations.py
+++ Blocks/Activations.py
@@ -3,15 +3,16 @@ import torch
 import math
 
 class GELU(nn.Module):
-    """
-    The GELU is similar to the ReLU (Rectified Linear Unit) activation function, but it has a smoother curve. 
-    The GELU function is defined as follows:
-
-    GELU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi) * (x + 0.044715 * x^3)))
-
-    This function is used to introduce non-linearity in the model, allowing it to learn more complex relationships between the inputs and outputs.
-    """
+    
     def __init__(self):
+        """
+        The GELU is similar to the ReLU (Rectified Linear Unit) activation function, but it has a smoother curve. 
+        The GELU function is defined as follows:
+
+        GELU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi) * (x + 0.044715 * x^3)))
+
+        This function is used to introduce non-linearity in the model, allowing it to learn more complex relationships between the inputs and outputs.
+        """
         super().__init__()
 
     def forward(self, x):
@@ -19,3 +20,17 @@ class GELU(nn.Module):
                           (math.sqrt(2 / math.pi) * 
                            (x + 0.044715 * x ** 3)))
 
+
+class SiLU(nn.Module):
+
+    def __init__(self):
+        """
+        The SiLU (Sigmoid Linear Unit) activation function is a non-linear function that is used in deep learning models. 
+        It is defined as follows:
+        SiLU(x) = x * (1 + exp(-x))
+        SiLU(x) = x * sigmoid(x)
+        """
+        super().__init__()
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
