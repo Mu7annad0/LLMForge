6857da42674bdb6f20dc590a4ace66ea1912ef36
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-10-28 21:34:35 +0300

    Masked multi head attention with Rotary positional embedding

--- Attentions/AttentionRoPE.py
+++ Attentions/AttentionRoPE.py
@@ -12,10 +12,10 @@ class MultiHeadAttentionRoPE(nn.Module):
         self.d_out = d_out
         self.num_heads = num_heads
         self.head_dim = d_out // num_heads
-        self.w_q = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
-        self.w_k = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
-        self.w_v = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
-        self.w = nn.Linear(d_out, d_in, bias=qkv, dtype=dtype)
+        self.q_proj = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
+        self.k_proj = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
+        self.v_proj = nn.Linear(d_in, d_out, bias=qkv, dtype=dtype)
+        self.out_proj = nn.Linear(d_out, d_in, bias=qkv, dtype=dtype)
         self.register_buffer(
             "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
         )
@@ -25,9 +25,9 @@ class MultiHeadAttentionRoPE(nn.Module):
         b, num_tokens, d_in = x.shape
 
         # Linear transformations and reshaping for multi-head attention
-        q = self.w_q(x).view(b, num_tokens, self.num_heads, self.head_dim) # Queries
-        k = self.w_k(x).view(b, num_tokens, self.num_heads, self.head_dim) # Keys
-        v = self.w_v(x).view(b, num_tokens, self.num_heads, self.head_dim) # Values
+        q = self.q_proj(x).view(b, num_tokens, self.num_heads, self.head_dim) # Queries
+        k = self.k_proj(x).view(b, num_tokens, self.num_heads, self.head_dim) # Keys
+        v = self.v_proj(x).view(b, num_tokens, self.num_heads, self.head_dim) # Values
         # shape now is --> [b, num_tokens, num_heads, head_dim]
 
         # apply rotary embedding to query and key
@@ -57,7 +57,7 @@ class MultiHeadAttentionRoPE(nn.Module):
         context_vector = context_vector.contiguous().view(
             b, num_tokens, self.d_out  # Reshape to (b, num_tokens, d_out)
         )
-        context_vector = self.w(context_vector)
+        context_vector = self.out_proj(context_vector)
         return context_vector
 
 
